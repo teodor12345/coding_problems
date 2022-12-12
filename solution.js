7688ce01bfa2f4475e4479c385845c929acf58f7
Teodor Madzovski <ted.madzovski@gmail.com>
2022-12-12 16:35:08 +0100

    3 commit

--- solution.js
+++ solution.js
@@ -1,13 +1,5 @@
-function checkPair(arr, k);
+function checkPair(arr,k);
 
-function findPair(arr, k){
-    for(let i = 0; i < arr.length; i++){
-      for(let j = i+1; j < arr.length; j++){
-        if(arr[i] + arr[j] == k) return true;
-      }
-    }
-    return false;
-  }
-  
+
   
   
